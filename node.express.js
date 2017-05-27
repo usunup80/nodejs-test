663c60a9affb3eac617da6f546a1e78e83b8fbd1
NAVER <naver@AL01219451.local>
2017-05-28 03:43:06 +0900

    second commit

--- node.express.js
+++ node.express.js
@@ -26,11 +26,43 @@ app.get('/list', function (request, response){
         console.log(item);
     });
 
+    var dateList = [];
+    var day = new Date('2017-03-01');
+    for(var i=0; i<21; i++){
+        var formattedDate = getFormattedDate(day);
+
+        console.log(formattedDate);
+        dateList.push(formattedDate);
+        day.setDate(day.getDate()+1);
+    }
+
     fs.readFile('list.html', 'utf-8', function(error, data) {
-        client.query('SELECT * FROM tb_student', function(error, results){
+        var aStudent = null;
+        client.query('select * from tb_student', function(error, results){
+            aStudent = results;
+        });
+
+        query = 'select attendance_date, student_num from tb_attendance_book ab';
+        client.query(query, function(error, results){
+            var oResult = {};
+            results.forEach(function (item, index){
+                //console.log(item.student_num, item.attendance_date);
+
+                var studentNum = item.student_num;
+                var attendanceDate = getFormattedDate(item.attendance_date);
+                if(!(attendanceDate in oResult)){
+                    oResult[attendanceDate] = [];
+                }
+                oResult[attendanceDate].push(studentNum);
+            });
+
+            console.log(oResult);
+
             response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
             response.end(ejs.render(data, {
-                data: results
+                data: oResult,
+                dateArr : dateList,
+                students: aStudent
             }));
         });
     });
@@ -47,3 +79,11 @@ var port = 52273;
 app.listen(port, function() {
     console.log('Server running at http://127.0.0.1:' + port + '/');
 });
+
+function getFormattedDate(day){
+    var nMonth = day.getMonth()+1;
+    var sMonth = nMonth < 10 ? '0'+nMonth: nMonth;
+    var sDate = day.getDate() < 10 ? '0'+day.getDate(): day.getDate();
+    var formattedDate = day.getFullYear()+'-'+sMonth+'-'+sDate;
+    return formattedDate;
+}
