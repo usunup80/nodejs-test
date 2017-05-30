c745c614bf93b0a83b5b01a1f7004d369ad49ac7
NAVER <naver@AL01219451.local>
2017-05-30 11:23:50 +0900

    post로 출첵하는 부분 추가

--- node.express.js
+++ node.express.js
@@ -16,22 +16,62 @@ var app = express();
 //     response.send([{'id': 1, 'name': '이영훈'},{'id': 2, 'name': '장재혁'}]);
 // });
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
 app.get('/list', function (request, response){
-    var arr = [{'id': 3, 'name': '이영훈'},{'id': 4, 'name': '장재혁'}];
-    arr.forEach(function (item, index) {
-        console.log(item);
+    // var arr = [{'id': 3, 'name': '이영훈'},{'id': 4, 'name': '장재혁'}];
+    // arr.forEach(function (item, index) {
+    //     console.log(item);
+    // });
+
+    makeResponse(request, response);
+});
+
+app.post('/attend/:studentNum', function (request, response){
+    var data = request.body.data;
+    //var studentNum = request.params.studentNum;
+
+    console.log(request.body);
+    //console.log(request.params);
+
+    console.log(JSON.parse(data));
+
+    var dataJson = JSON.parse(data);
+    console.log(dataJson.studentNum);
+
+    var sStudentNum = dataJson.studentNum;
+
+    var query = 'INSERT INTO tb_attendance_book (attendance_date, student_num) values(NOW(), '+ sStudentNum +')';
+    client.query(query, function(error, results){
+        console.log(results);
     });
 
+    response.redirect('/list');
+});
+
+var port = 52273;
+app.listen(port, function() {
+    console.log('Server running at http://127.0.0.1:' + port + '/');
+});
+
+function getFormattedDate(day){
+    var nMonth = day.getMonth()+1;
+    var sMonth = nMonth < 10 ? '0'+nMonth: nMonth;
+    var sDate = day.getDate() < 10 ? '0'+day.getDate(): day.getDate();
+    var formattedDate = day.getFullYear()+'-'+sMonth+'-'+sDate;
+    return formattedDate;
+}
+
+function makeResponse(request, response){
     var dateList = [];
     var day = new Date('2017-03-01');
     for(var i=0; i<21; i++){
         var formattedDate = getFormattedDate(day);
 
-        console.log(formattedDate);
+        //console.log(formattedDate);
         dateList.push(formattedDate);
         day.setDate(day.getDate()+1);
     }
@@ -56,7 +96,7 @@ app.get('/list', function (request, response){
                 oResult[attendanceDate].push(studentNum);
             });
 
-            console.log(oResult);
+            //console.log(oResult);
 
             response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
             response.end(ejs.render(data, {
@@ -66,24 +106,4 @@ app.get('/list', function (request, response){
             }));
         });
     });
-
-});
-
-app.post('/attend/:id', function (request, response){
-    var id = request.params.id;
-
-    response.send(id);
-});
-
-var port = 52273;
-app.listen(port, function() {
-    console.log('Server running at http://127.0.0.1:' + port + '/');
-});
-
-function getFormattedDate(day){
-    var nMonth = day.getMonth()+1;
-    var sMonth = nMonth < 10 ? '0'+nMonth: nMonth;
-    var sDate = day.getDate() < 10 ? '0'+day.getDate(): day.getDate();
-    var formattedDate = day.getFullYear()+'-'+sMonth+'-'+sDate;
-    return formattedDate;
 }
